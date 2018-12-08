6c890700cf0304a1741971e63650895c4cde0423
squareGITHUB <44121785+squareGITHUB@users.noreply.github.com>
2018-12-08 22:54:42 +0000

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,8 +1,9 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (client, message, args) => {
+    let help = message.content.slice(prefix.length).trim().split(" ");
     if (message.author.bot) return;
-    if (!args) {
+    if (!help) {
     let serverembed = new Discord.RichEmbed()
     .setColor(Math.floor(Math.random() * 16777214) + 1)
     .setTitle("Commands:")
@@ -11,25 +12,25 @@ module.exports.run = async (client, message, args) => {
     .addField("Roles:", "`>_addrole`, `>_removerole`");
     return message.channel.send(serverembed);
     }
-    if (args.includes("kick")) {
+    if (help.includes("kick")) {
         message.channel.send("**Kick:** A command that kicks an user. Usage: >_kick (user)")
     } else {
-    if (args.includes("ban")) {
+    if (help.includes("ban")) {
         message.channel.send("**Ban:** A command that bans an user. Usage: >ban (user) (reason)")
     } else {
-    if (args.includes("mute")) {
+    if (help.includes("mute")) {
         message.channel.send("**Mute:** A command that mutes an user from typing. Usage: >mute (user) (time)")
     } else {
-    if (args.includes("blind")) {
+    if (help.includes("blind")) {
         message.channel.send("**Blind:** A command that blinds an user from typing or seeing future messages. Usage: >blind (user) (time)")
     } else {
-    if (args.includes("addrole")) {
+    if (help.includes("addrole")) {
         message.channel.send("**Addrole:** A command that adds a role to an user. Usage: >_addrole (user) (role name)")
     } else {
-    if (args.includes("removerole")) {
+    if (help.includes("removerole")) {
         message.channel.send("**Removerole:** A command that removes a role from an user. Usage: >removerole (user) (rolename)")
     } else {
-        if (args) {
+        if (help) {
 message.channel.send("That isn't a command.")
 }}}}}}}
 
