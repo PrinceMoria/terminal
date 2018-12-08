2479f741e6fbf2882946a6ecb1f1ef943eac6497
jsinitx4 <43330808+jsinitx4@users.noreply.github.com>
2018-12-08 12:55:55 -0500

    command handler

--- bot.js
+++ bot.js
@@ -1,2 +1,41 @@
+const Discord = require("discord.js");
+const client = new Discord.Client({disableEveryone: true});
+const config = require("./config.json");
+const fs = require("fs");
 
-test
+client.commands = new Discord.Collection();
+fs.readdir("./commands/", (err, files) => {
+  if (err) console.error(err);
+  let jsfiles = files.filter(f => f.split(".").pop() === "js");
+
+  if (jsfiles.length <= 0) return console.log("no commands to load");
+
+  console.log(`loading ${jsfiles.length} commands...`);
+  jsfiles.forEach((f, i) => {
+    let props = require(`./commands/${f}`);
+    console.log(`${f} loaded!`);
+    client.commands.set(props.help.name, props);
+  });
+});
+
+client.on('ready', () => {
+  client.user.setActivity("your commands", { type: 'WATCHING' })
+  console.log(`ready to instantly die`);
+});
+
+client.on('message', message => {
+  if (message.author.bot) return;
+  if (message.channel.type === "dm") return;
+
+  let prefix = config.prefix;
+  let messageArray = message.content.split(" ");
+  let command = messageArray[0].toLowerCase();
+  let args = messageArray.slice(1);
+
+  if (!command.startsWith(prefix)) return;
+
+  let cmd = client.commands.get(command.slice(prefix.length));
+  if (cmd) cmd.run(client, message, args, ops);
+});
+
+client.login("not for your eyes, no");
