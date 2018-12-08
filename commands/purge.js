af0255bc33fd71b489092c800d764ce8e4a5136c
squareGITHUB <44121785+squareGITHUB@users.noreply.github.com>
2018-12-08 19:49:18 +0000

    changed messages

--- commands/purge.js
+++ commands/purge.js
@@ -1,10 +1,11 @@
 const Discord = require('discord.js');
 
 module.exports.run = (client, message, args) => {
-    if (!message.member.hasPermission('MANAGE_MESSAGES')) return message.channel.send("You cannot perform this command, invalid permissions.")
-  if(isNaN(args[0])) return message.channel.send("Supply an interger please");
-  if (args[0] > 100) return message.channel.send("Sadly, I can't purge any messages over 100. Please choose a smaller interger.");
+    if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send(":x: You do not have sufficient permissions to purge messages.");
+  if(isNaN(args[0])) return message.channel.send(":x: Supply an interger please");
+  if (args[0] > 100) return message.channel.send(":x: I can't purge more than 100 messages.");
   message.channel.bulkDelete(args[0]);
+  message.channel.send(":white_check_mark: Successfully deleted " + args[0] + " messages.")
 }
 module.exports.help = {
     name: "purge"
