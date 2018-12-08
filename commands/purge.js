e07fb6ce57c41f9245f53592476b2772cd110a44
squareGITHUB <44121785+squareGITHUB@users.noreply.github.com>
2018-12-08 20:46:42 +0000

    overhaul layout

--- commands/purge.js
+++ commands/purge.js
@@ -1,11 +1,11 @@
 const Discord = require('discord.js');
 
 module.exports.run = (client, message, args) => {
-    if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("**/" + message.guild + "/" + message.channel + "/**" + " You do not have sufficient permissions to purge messages.");
-  if(isNaN(args[0])) return message.channel.send("**/" + message.guild + "/" + message.channel + "/**" + " Supply an interger please");
-  if (args[0] > 100) return message.channel.send("**/" + message.guild + "/" + message.channel + "/**" + " I can't purge more than 100 messages.");
+    if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("**/" + message.guild + "/" + message.channel.name + "/**" + " You do not have sufficient permissions to purge messages.");
+  if(isNaN(args[0])) return message.channel.send("**/" + message.guild + "/" + message.channel.name + "/**" + " Supply an interger please");
+  if (args[0] > 100) return message.channel.send("**/" + message.guild + "/" + message.channel.name + "/**" + " I can't purge more than 100 messages.");
   message.channel.bulkDelete(args[0]);
-  message.channel.send("**/" + message.guild + "/" + message.channel + "/**" + " Successfully deleted " + args[0] + " messages.")
+  message.channel.send("**/" + message.guild + "/" + message.channel.name + "/**" + " Successfully deleted " + args[0] + " messages.")
 }
 module.exports.help = {
     name: "purge"
