0a6478c57077bc3fa0a503c0b4c8ee363854ab1e
squareGITHUB <44121785+squareGITHUB@users.noreply.github.com>
2018-12-08 19:53:28 +0000

    Update addrole.js

--- commands/addrole.js
+++ commands/addrole.js
@@ -2,19 +2,19 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-  if(!message.member.hasPermission("MANAGE_MEMBERS")) return message.channel.send("Invalid permissions");
+  if (!message.member.hasPermission("MANAGE_MEMBERS")) return message.channel.send(":x: You do not have sufficient permissions to purge messages.");
   let rMember = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
-  if(!rMember) return message.channel.send("Invalid user, couldn't find that user");
+  if(!rMember) return message.channel.send(":x: Couldn't find that user");
   let role = args.join(" ").slice(22);
-  if(!role) return message.channel.send("Please supply a role");
+  if(!role) return message.channel.send(":x: Please supply a role");
   let gRole = message.guild.roles.find(`name`, role);
-  if(!gRole) return message.channel.send("`Hmmmm` That role doesn't exist, check your spelling");
+  if(!gRole) return message.channel.send(":x: That role doesn't exist, if it does exist: check your spelling");
 
-  if(rMember.roles.has(gRole.id)) return message.channel.send("This person already has this role");
+  if(rMember.roles.has(gRole.id)) return message.channel.send(":x: This person already has that role.");
   await(rMember.addRole(gRole.id));
 
   try{
-    await message.channel.send(`Done added ${gRole.name} to ${rMember}`)
+    await message.channel.send(`:white_check_mark: Added ${gRole.name} to ${rMember}`)
   }catch(e){
     console.log(e.stack);
   }
