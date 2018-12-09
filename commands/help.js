d8275196921beba0a8053c44f4f81a003b2b39a7
squareGITHUB <44121785+squareGITHUB@users.noreply.github.com>
2018-12-09 13:33:50 +0000

    Revert "more updates etc etc"
    This reverts commit 188fffa2523aa8ec117f69149df4692f23dc8e29.

--- commands/help.js
+++ commands/help.js
@@ -17,9 +17,6 @@ module.exports.run = async (client, message, args) => {
     if (args.includes("addrole")) {
         message.channel.send("**Addrole:** A command that adds a role to an user. Usage: >_addrole (user) (role name)")
     } else {
-    if (args.includes("developers")) {
-        message.channel.send("**Developers:** Displays the bot owners/developers")
-    } else {
     if (args.includes("removerole")) {
         message.channel.send("**Removerole:** A command that removes a role from an user. Usage: >removerole (user) (rolename)")
     } else {
@@ -28,8 +25,7 @@ module.exports.run = async (client, message, args) => {
         .setTitle("Commands:")
         .setDescription("Terminal: A moderation bot. Do >_help (command) to get more info.")
         .addField("Punishment:", "`>_kick`, `>_ban`, `>_mute`, `>_blind`")
-        .addField("Roles:", "`>_addrole`, `>_removerole`")
-        .addfield("General", "`>_developers`");
+        .addField("Roles:", "`>_addrole`, `>_removerole`");
      message.channel.send(serverembed);
 }}}}}}
     
