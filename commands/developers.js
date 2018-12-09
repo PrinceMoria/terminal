d2065732fbb6dedec93c9a9086f144a884fe8130
squareGITHUB <44121785+squareGITHUB@users.noreply.github.com>
2018-12-09 13:59:54 +0000

    Revert "more commands etc etc"
    This reverts commit e5f9d6ac3b28a46eb4f3ff260baaf51b6926e225.

--- commands/developers.js
+++
@@ -1,9 +0,0 @@
-const Discord = require("discord.js");
-
-module.exports.run = async (client, message, args) => {
-    message.channel.send("The bot developers are `square#1255`, and `speed#5496`");
-};
-
-module.exports.help = {
-    name: "developers"
-};
