92cdf566edd374c9a91a0280d5e19eac718fcf86
speed <43330808+jsinitx4@users.noreply.github.com>
2018-12-09 12:39:45 -0500

    more additions etc etc

---
+++ commands/autorole.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+
+exports.run = async (bot, message, args) => {
+	if (!message.member.hasPermission("MANAGE_ROLES")) return message.channel.send("You do not have permissions to use this command.");
+	let autorole = JSON.parse(fs.readFileSync("./autorole.json", "utf8"));
+	if (!args[0]) { 
+		autorole[message.guild.id] = {
+			role: 0
+		};
+		fs.writeFile("./autorole.json", JSON.stringify(autorole), (err) => {
+			if (err) console.log(err);
+		});
+		message.channel.send("Autorole has been turned off");
+	}
+	if (args[0]) { 
+		let roles = args.join(" ");
+		let role = message.guild.roles.find("name", roles);
+		autorole[message.guild.id] = {
+			role: role.id 
+		};
+		fs.writeFile("./autorole.json", JSON.stringify(autorole), (err) => {
+			if (err) console.log(err)
+		});
+		message.channel.send(`The server autorole set to **${role.name}**`);
+	}
+}
