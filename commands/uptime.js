dadd390f6f62bcfbedea64baf831ecd0eb4140ac
speed <43330808+jsinitx4@users.noreply.github.com>
2018-12-09 13:08:31 -0500

    fix

--- commands/uptime.js
+++ commands/uptime.js
@@ -1,10 +1,10 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-    let days = Math.floor(moist.uptime / 86400000);
-    let hours = Math.floor(moist.uptime / 3600000) % 24;
-    let minutes = Math.floor(moist.uptime / 60000) % 60;
-    let seconds = Math.floor(moist.uptime / 1000) % 60;
+    let days = Math.floor(client.uptime / 86400000);
+    let hours = Math.floor(client.uptime / 3600000) % 24;
+    let minutes = Math.floor(client.uptime / 60000) % 60;
+    let seconds = Math.floor(client.uptime / 1000) % 60;
 
      message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + `uptime: ${days}d ${hours}h ${minutes}m ${seconds}s`);
   }
