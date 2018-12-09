555985a8834aea12286561c1c5599db57bb10d8e
squareGITHUB <44121785+squareGITHUB@users.noreply.github.com>
2018-12-09 14:14:58 +0000

    final final fix

--- commands/developers.js
+++ commands/developers.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (client, message, args) => {
-    message.channel.send("The developers of Terminal are: `square#1255`, and `speed#5496`");
+    message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + "The developers of Terminal are: `square#1255`, and `speed#5496`");
 };
 
 module.exports.help = {
--- commands/help.js
+++ commands/help.js
@@ -4,34 +4,35 @@ module.exports.run = async (client, message, args) => {
     if (message.author.bot) return;
 
     if (args.includes("kick")) {
-        message.channel.send("**Kick:** A command that kicks an user. Usage: >_kick (user)")
+        message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + "**Kick:** A command that kicks an user. Usage: >_kick (user)")
     } else {
     if (args.includes("ban")) {
-        message.channel.send("**Ban:** A command that bans an user. Usage: >_ban (user) (reason)")
+        message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + "**Ban:** A command that bans an user. Usage: >_ban (user) (reason)")
     } else {
     if (args.includes("mute")) {
-        message.channel.send("**Mute:** A command that mutes an user from typing. Usage: >_mute (user) (time)")
+        message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + "**Mute:** A command that mutes an user from typing. Usage: >_mute (user) (time)")
     } else {
     if (args.includes("blind")) {
-        message.channel.send("**Blind:** A command that blinds an user from typing or seeing future messages. Usage: >_blind (user) (time)")
+        message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + "**Blind:** A command that blinds an user from typing or seeing future messages. Usage: >_blind (user) (time)")
     } else {
     if (args.includes("addrole")) {
-        message.channel.send("**Addrole:** A command that adds a role to an user. Usage: >_addrole (user) (role name)")
+        message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + "**Addrole:** A command that adds a role to an user. Usage: >_addrole (user) (role name)")
     } else {
     if (args.includes("removerole")) {
-        message.channel.send("**Removerole:** A command that removes a role from an user. Usage: >_removerole (user) (rolename)")
+        message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + "**Removerole:** A command that removes a role from an user. Usage: >_removerole (user) (rolename)")
     } else {
      if (args.includes("developers")) {
-        message.channel.send("**Developers:** A command that shows the developers and owners of Terminal. Usage: >_developers")
+        message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + "**Developers:** A command that shows the developers and owners of Terminal. Usage: >_developers")
     } else {
         let serverembed = new Discord.RichEmbed()
         .setColor(Math.floor(Math.random() * 16777214) + 1)
-        .setAuthor('Terminal')
+        .setAuthor('Terminal Panel')
         .setTitle("Commands:")
         .setDescription("Terminal: A moderation bot. Do >_help (command) to get more info.")
         .addField("Punishment:", "`>_kick`, `>_ban`, `>_mute`, `>_blind`")
         .addField("Roles:", "`>_addrole`, `>_removerole`")
         .addField("Other:", "`>_developers`, `>_help`");
+     message.channel.send("**/" + message.guild + "/" + message.channel.name + "/**")
      message.channel.send(serverembed);
 }}}}}}}
     
