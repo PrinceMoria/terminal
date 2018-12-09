4ecbf571e8d9d7ecf81edf74900050287e013890
squareGITHUB <44121785+squareGITHUB@users.noreply.github.com>
2018-12-09 16:43:48 +0000

    update

--- commands/softban.js
+++ commands/softban.js
@@ -3,15 +3,16 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
     if (args.includes("@everyone")) return message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + ' **Error**');
     if (args.includes("@here")) return message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + ' **Error** ');
-    if (!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + " You do not have sufficient permissions to ban this player.");
+    if (!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + " You do not have sufficient permissions to softban this player.");
     let user = message.mentions.users.first();
-    if (!user) return message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + " Please mention a user to ban.");
+    if (!user) return message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + " Please mention a user to softban.");
 
     let reason = args.join(" ");
     if (!reason) reason = " Insufficient reason.";
 
     message.guild.member(user).ban(reason);
-    message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + " Successfully banned user: " + user + ", for the reason: " + reason)
+    message.guild.unban(user);
+    message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + " Successfully softbanned user: " + user + ", for the reason: " + reason)
 }
 module.exports.help = {
     name: "ban"
