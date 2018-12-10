86f2963e0543a0b95d320ae1ba62d45bacd7d311
squareGITHUB <44121785+squareGITHUB@users.noreply.github.com>
2018-12-10 20:52:59 +0000

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -2,7 +2,9 @@ const Discord = require("discord.js");
 
 module.exports.run = async (client, message, args) => {
     if (message.author.bot) return;
-    message.channel.send("Pinging...").then((message)=>{
+    let pings = ["Loading...", "Waiting...", "Pinging...", "Doing a thing...", "Debugging...", "Shopping...", "Coding...", "Changing language...", "Doing nothing...", "Doing something...", "Doing things...", ":ok_hand:...", "Alright...", "Uh...", "404..."]
+    var ping = pings[Math.floor(Math.random() * pings.length)];
+    message.channel.send(ping).then((message)=>{
     message.edit("**/" + message.guild + "/" + message.channel.name + "/** \n  " + `Pinged! // \`${client.pings[0]}ms\``);
     })
 };
