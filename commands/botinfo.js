818172f97ab6f8803aeb4320b9c4ec1f42e9c010
speed <43330808+jsinitx4@users.noreply.github.com>
2018-12-11 17:53:20 -0500

    flex on square

---
+++ commands/botinfo.js
@@ -0,0 +1,24 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (client, msg, args) => {
+
+let bicon = bot.user.displayAvatarURL;
+let bembed = new Discord.RichEmbed()
+      .setColor(Math.floor(Math.random() * 16777214) + 1)
+      .setThumbnail(bicon)
+      .addField("Bot name", client.user.username)
+      .addField("Bot member size", client.users.size)
+      .addField("Bot guild size", client.guilds.size)
+      .addField("RAM Usage", `${((process.memoryUsage().heapUsed / 1024) / 1024).toFixed(2)} MB`)
+      .addField("Created on", client.user.createdAt)
+      .addField(`Discord.js version`, `Discord JS v${Discord.version}`)
+      .addField("Node version", `${process.version}`)
+      .addField("Bot developers", "square#1255, speed#5496");
+       msg.channel.send("**/" + msg.guild + "/" + msg.channel.name + "/**")
+       msg.channel.send(bembed);
+    }
+
+module.exports.help = {
+    name: "botinfo"
+}
+  
