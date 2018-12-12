0dea694088e5fea4df367879f9bde436c236b862
speed <43330808+jsinitx4@users.noreply.github.com>
2018-12-12 17:09:31 -0500

    more commands etc etc

---
+++ commands/userinfo.js
@@ -0,0 +1,19 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (client, message, args) => { 
+
+let user = message.mentions.users.first()
+const userInfo = new Discord.RichEmbed()
+            .setAuthor('Info For ' + user.username)
+            .setColor('#2D7FFF')
+            .setThumbnail(user.avatarURL)
+            .addField('Discriminator', user.discriminator)
+            .addField('Status', user.presence.status, true)
+            .addField('Join Date', message.member.joinedAt)
+            .addField('Created Account', user.createdAt)
+        message.channel.send("**/" + message.guild + "/" + message.channel.name + "/**")
+        message.channel.send({ embed: userInfo })
+    }
+    module.exports.help = {
+        name: "userinfo"
+    };
