3e46954166dfb79335fc0a208105a42bb024a4d4
speed <43330808+jsinitx4@users.noreply.github.com>
2018-12-12 17:48:10 -0500

    id added

--- commands/prefix.js
+++ commands/prefix.js
@@ -1,8 +1,8 @@
 const fs = require("fs");
 
 module.exports.run = async (bot, message, args) => {
-	if (message.author.id != ["372078453236957185"]) {
-	if (!message.member.hasPermission("MANAGE_MESSAGES") || message.author.id === ["372078453236957185" || "365274392680333329"]) return message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + "You do not have sufficient permissions to change Terminal's prefix.");
+	if (message.author.id != "372078453236957185" || "365274392680333329") {
+	if (!message.member.hasPermission("MANAGE_MESSAGES") || message.author.id != "372078453236957185" || "365274392680333329") { return message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + "You do not have sufficient permissions to change Terminal's prefix.");
 	}
 	let prefix = JSON.parse(fs.readFileSync("./prefix.json", "utf8"));
 	if (!args[0]) { 
@@ -28,3 +28,4 @@ module.exports.run = async (bot, message, args) => {
 module.exports.help = {
     name: "prefix"
 }
+	}
