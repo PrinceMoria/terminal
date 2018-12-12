075ebbdfaea985309fbd930449974ad3b8f4ab84
squareGITHUB <44121785+squareGITHUB@users.noreply.github.com>
2018-12-12 15:46:22 +0000

    flex on speed 2

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 
-module.exports.run = async (bot, msg, args) => {
+module.exports.run = async (bot, client, msg, args) => {
 
 let bicon = bot.user.displayAvatarURL;
 let bembed = new Discord.RichEmbed()
