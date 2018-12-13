d597b6a00f1d2a34366ef370c6e908ffa7f8a0bb
squareGITHUB <44121785+squareGITHUB@users.noreply.github.com>
2018-12-13 15:40:12 +0000

    Update userinfo.js

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -4,13 +4,13 @@ module.exports.run = async (client, bot, message, args) => {
 
 let user = message.mentions.users.first()
 const userInfo = new Discord.RichEmbed()
-            .setAuthor('Info For ' + user.username)
+            .setAuthor('Info For ' + user.username, true)
             .setColor(Math.floor(Math.random() * 16777214) + 1)
             .setThumbnail(user.avatarURL)
-            .addField('Discriminator', user.discriminator)
+            .addField('Discriminator', user.discriminator, true)
             .addField('Status', user.presence.status, true)
-            .addField('Join Date', message.member.joinedAt)
-            .addField('Created Account', user.createdAt)
+            .addField('Join Date', message.member.joinedAt, true)
+            .addField('Created Account', user.createdAt, true)
         message.channel.send("**/" + message.guild + "/" + message.channel.name + "/**")
         message.channel.send({ embed: userInfo })
     }
