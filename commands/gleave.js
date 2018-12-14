76e25aacead0bee536b77af1913bdd17441dbda1
speed <43330808+jsinitx4@users.noreply.github.com>
2018-12-14 16:15:26 -0500

    bot admin commands superior

---
+++ commands/gleave.js
@@ -0,0 +1,10 @@
+module.exports.run = (client, message, args, ops) => {
+    if (args.length  < 1) return message.reply("**/" + message.guild + "/" + message.channel.name + "/** \n  " + "Please supply a server ID.");
+    if (message.author.id !== "372078453236957185")
+    if (message.author.id !== "365274392680333329") return message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + "You are not a Terminal developer.")
+   client.guilds.get(args.join(" ")).leave()
+   .then(g => message.channel.send(("**/" + message.guild + "/" + message.channel.name + "/** \n  " + `Left ${g} successfully`)) .catch(console.error)); 
+    }
+	module.exports.help = {
+    name: "gleave"
+}
