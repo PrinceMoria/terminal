b4846a5b36c4e523012d0541a1dab5e0a4fa70f0
squareGITHUB <44121785+squareGITHUB@users.noreply.github.com>
2018-12-14 21:27:29 +0000

    Update gleave.js

--- commands/gleave.js
+++ commands/gleave.js
@@ -1,5 +1,5 @@
 module.exports.run = (client, message, args, ops) => {
-    if (args.length  < 1) return message.reply("**/" + message.guild + "/" + message.channel.name + "/** \n  " + "Please supply a server ID.");
+    if (args.length  < 1) return message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + "Please supply a server ID.");
     if (message.author.id !== "372078453236957185")
     if (message.author.id !== "365274392680333329") return message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + "You are not a Terminal developer.")
    client.guilds.get(args.join(" ")).leave()
