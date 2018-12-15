8b05d13dedda4b0e277dfc5e44d896f182461299
squareGITHUB <44121785+squareGITHUB@users.noreply.github.com>
2018-12-15 16:39:59 +0000

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -7,7 +7,6 @@ module.exports.run = async (bot, message, args) => {
     if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + " You do not have sufficient permissions to kick this user.");
     let tokick = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
     if (!tokick) return message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + " Couldn't find user.");
-    if (tokick.hasPermission("MANAGE_MESSAGES")) return message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + " The user you are trying to kick is either the same, or higher ranking than you.");
     var member = message.mentions.members.first();
     member.kick().then((member) => {
         message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + " " + member.displayName + " has been kicked by " + message.author.username);
