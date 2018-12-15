5e63c9a4587f3478f116a552363363408575ab1b
speed <43330808+jsinitx4@users.noreply.github.com>
2018-12-15 12:44:28 -0500

    this took forever
    m

---
+++ commands/pin.js
@@ -0,0 +1,19 @@
+const Discord = require("discord.js");
+
+module.exports.run = (bot, message, args, discord) => {
+    let nid = args.join(' ');
+    if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + "You do not have sufficient permissions to pin messages.");
+    if (args.length < 1) return message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + "Please supply an ID to ban.")
+      message.channel.fetchMessage(nid).then(id => {
+        id.pin().catch(err => {
+          message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + "Failed to pin" + id)
+          console.log(err)
+        })
+        message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " +  `Successfully pinned **${id}**.`)
+      }).catch(() => {
+        message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + `${nid} isn't a real message ID. Please enter a valid ID.`)
+      })
+  }
+  module.exports.help = {
+    name: "pin"
+}
