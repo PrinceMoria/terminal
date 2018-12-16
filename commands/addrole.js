eeea0783f85d93fe74a694364aea0d646365ea63
speed <43330808+jsinitx4@users.noreply.github.com>
2018-12-15 21:09:19 -0500

    fix

--- commands/addrole.js
+++ commands/addrole.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-  if (!message.member.hasPermission("MANAGE_MEMBERS")) return message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + " You do not have sufficient permissions to purge messages.");
+  if (!message.member.hasPermission("MANAGE_MEMBERS")) return message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + " You do not have sufficient permissions to add roles.");
   let rMember = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
   if(!rMember) return message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n " + "  Couldn't find that user");
   let role = args.join(" ").slice(22);
